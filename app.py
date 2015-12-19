5e2e7917169f814c4a61443235352bed6c1531dc
Jw <jwaltonmedia@gmail.com>
2015-12-19 12:49:03 -0600

    debugging

--- app.py
+++ app.py
@@ -31,4 +31,4 @@ def test_disconnect():
     print('Client disconnected...')
 
 if __name__ == "__main__":
-    socketio.run(app, host='0.0.0.0', port=80)
+    socketio.run(app, host='0.0.0.0', port=80, debug=True)
